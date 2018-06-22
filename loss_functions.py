Bugfix KL Divergence
Should be correct now, and free of nan.

loss_functions.py
@@ -33,7 +33,7 @@ def weighted_cross_entropy_loss(out,truth_vec,class_frequency,epsilon):
 
 	
 def kl_divergence_loss(out,truth_vec,epsilon):
-	return tf.reduce_sum(out*tf.log(out/tf.minimum(1.0,truth_vec+epsilon)))
+	return tf.reduce_sum(truth_vec*tf.log(tf.minimum(1.0,truth_vec/tf.minimum(1.0,out+epsilon)+epsilon)))
 	
 	
 def segmentation_accuracy(score_map,target,predict0):